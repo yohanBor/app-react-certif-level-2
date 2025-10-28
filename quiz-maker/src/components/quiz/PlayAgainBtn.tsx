50cb6a898d35640b8f1d97bce8ace0fa1e9b1f9e
Yohan BORNES <yohan.bornes@soprasteria.com>
2025-10-28 13:37:13 +0100

    update text

--- quiz-maker/src/components/quiz/PlayAgainBtn.tsx
+++ quiz-maker/src/components/quiz/PlayAgainBtn.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 const PlayAgainBtn = () => {
     const { resetQuiz } = useQuizContext();
 
-    return <Link to={"/"} className="border border-neutral-300 p-2 rounded-xs hover:bg-neutral-300" onClick={resetQuiz}>Play again</Link>
+    return <Link to={"/"} className="border border-neutral-300 p-2 rounded-xs hover:bg-neutral-300" onClick={resetQuiz}>Create a new quiz</Link>
 };
 
 export default PlayAgainBtn;
